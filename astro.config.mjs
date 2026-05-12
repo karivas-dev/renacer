6c1af98bbf463387c0dd364396a67bd08957d954
Karen Rivas <karenrivasg17@gmail.com>
2026-05-11 21:17:47 -0600

    chore: setup project dependencies (tailwind, vue, lucide, unovis)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,28 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  prefetch: true,
+
+  image: {
+      service: {
+          entrypoint: 'astro/assets/services/sharp',
+          config: {
+              jpeg: {
+                  mozjpeg: true,
+                  progressive: true,
+              }
+          }
+      }
+  },
+
+  integrations: [vue()],
+});
